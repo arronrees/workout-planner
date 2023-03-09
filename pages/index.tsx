34ab79f2e69d35e0cadc2e1cb5af23dabd7aa154
arronrees <64908445+arronrees@users.noreply.github.com>
2023-03-09 19:38:21 +0000

    Added initial auth0 setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 export default function Home() {
-  return <></>;
+  const { user } = useUser();
+
+  console.log(user);
+
+  return (
+    <>
+      <h1>Workout Planner</h1>
+      <a href='/api/auth/login'>Login</a>
+      <a href='/api/auth/logout'>Logout</a>
+    </>
+  );
 }

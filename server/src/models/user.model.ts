739886c2f2e3c50739dd4ca05db89446028fc985
arronrees <64908445+arronrees@users.noreply.github.com>
2023-05-06 16:51:04 +0100

    Created signup user model with zod

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const signupUserModel = z.object({
+  name: z.string({
+    required_error: 'Name is required',
+    invalid_type_error: 'Name must be a string',
+  }),
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email(),
+  password: z.string({
+    required_error: 'Password is required',
+    invalid_type_error: 'Password must be a string',
+  }),
+});
+
+export type SignupUserType = z.infer<typeof signupUserModel>;

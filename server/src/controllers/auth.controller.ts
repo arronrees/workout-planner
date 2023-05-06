f4ac9db6c6d42b82a5189ee93d604e9168b75b00
arronrees <64908445+arronrees@users.noreply.github.com>
2023-05-06 21:18:14 +0100

    Added email service with verification email added to signup

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import { SignupUserType } from '../models/user.model';
 import { prismaDB } from '..';
 import { createJwtToken, hashPassword } from '../utils/auth.utils';
 import { omit } from 'lodash';
+import emailService from '../services/email.service';
+import randomstring from 'randomstring';
 
 // POST /signup
 export async function signupUserController(
@@ -28,12 +30,22 @@ export async function signupUserController(
     // user exists, create password hash
     const hash = await hashPassword(user.password);
 
+    const randomString = randomstring.generate();
+
     const newUser = await prismaDB.user.create({
-      data: { ...user, password: hash },
+      data: { ...user, password: hash, emailVerificationString: randomString },
     });
 
     // generate token
     const token = createJwtToken(newUser.id);
+
+    // send verification email
+    const verificationEmailMessage = await emailService.sendEmailVerification(
+      newUser.email,
+      newUser.id,
+      newUser.name,
+      randomString
+    );
 
     return res.status(200).json({
       success: true,

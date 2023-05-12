5af548614dad319ffa62be8de935ba4914683676
arronrees <64908445+arronrees@users.noreply.github.com>
2023-05-12 20:57:02 +0100

    Added signin page to frontend

--- web/pages/auth/signin.tsx
+++ web/pages/auth/signin.tsx
@@ -10,20 +10,18 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 
 type FormInputs = {
-  name: string;
   email: string;
   password: string;
 };
 
 type FormData = {
   user: {
-    name: string;
     email: string;
     password: string;
   };
 };
 
-export default function SignUp() {
+export default function SignIn() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const router = useRouter();
@@ -33,13 +31,12 @@ export default function SignUp() {
 
     const formData: FormData = {
       user: {
-        name: data.name,
         email: data.email,
         password: data.password,
       },
     };
 
-    const res = await fetch('/api/auth/signup', {
+    const res = await fetch('/api/auth/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
@@ -70,7 +67,6 @@ export default function SignUp() {
     formState: { errors },
   } = useForm<FormInputs>({
     defaultValues: {
-      name: '',
       email: '',
       password: '',
     },
@@ -80,15 +76,15 @@ export default function SignUp() {
     <Layout>
       <section>
         <h1 className='font-extralight text-4xl text-center mb-4'>
-          Let&apos;s get moving
+          Keep on moving
         </h1>
         <p className='font text-center mb-6 text-grey'>
-          Sign up to get started tracking your workouts and reaching your goals
+          Sign in to keep on reaching your goals
         </p>
         <p className='text-center font-extralight text-sm'>
-          Already have an account?{' '}
-          <Link href='/auth/signin' className='font-semibold'>
-            Sign In
+          Need have an account?{' '}
+          <Link href='/auth/signup' className='font-semibold'>
+            Sign Up
           </Link>
         </p>
       </section>
@@ -97,19 +93,6 @@ export default function SignUp() {
 
       <section className='mt-8'>
         <form className='grid gap-4' onSubmit={handleSubmit(handleFormSubmit)}>
-          <FormInputText labelText='Name' inputId='name'>
-            <>
-              <input
-                type='text'
-                id='name'
-                {...register('name', { required: 'Name is required' })}
-                className='form__input'
-              />
-              {errors.name?.message && (
-                <p className='form__error'>{errors.name?.message}</p>
-              )}
-            </>
-          </FormInputText>
           <FormInputText labelText='Email' inputId='email'>
             <>
               <input
@@ -150,7 +133,7 @@ export default function SignUp() {
           </FormInputText>
 
           <FormButton
-            text='Sign Up'
+            text='Sign In'
             btnClass='btn--green'
             disabled={isLoading}
           />

dfc2b1324eb3a6c8aab6dc25cfe9730381edc814
arronrees <64908445+arronrees@users.noreply.github.com>
2023-05-21 12:22:57 +0100

    Added update user details handlers

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { JsonApiResponse, ResLocals } from '../constant-types';
-import { UserPasswordUpdateType } from '../models/user.model';
+import {
+  UserDetailsUpdateType,
+  UserPasswordUpdateType,
+} from '../models/user.model';
 import { comparePassword, hashPassword } from '../utils/auth.utils';
 import { prismaDB } from '..';
 import emailService from '../services/email.service';
+import randomstring from 'randomstring';
 
 // PUT /update/password
 export async function updateUserPasswordController(
@@ -49,6 +53,63 @@ export async function updateUserPasswordController(
         email: currentUser.email,
         name: currentUser.name,
       });
+
+    return;
+  } catch (err) {
+    console.error(err);
+
+    next(err);
+  }
+}
+
+// PUT /update/details
+export async function updateUserDetailsController(
+  req: Request,
+  res: Response<JsonApiResponse> & { locals: ResLocals },
+  next: NextFunction
+) {
+  try {
+    const { user: currentUser } = res.locals;
+    const { user }: { user: UserDetailsUpdateType } = req.body;
+
+    if (!currentUser) {
+      return res.status(404).json({ success: false, error: 'User not found' });
+    }
+
+    // is email new?
+    const isEmailNew: boolean = user.email !== currentUser.email;
+
+    const isEmailRegistered = await prismaDB.user.findUnique({
+      where: { email: user.email },
+    });
+
+    if (isEmailRegistered) {
+      return res
+        .status(400)
+        .json({ success: false, error: 'Email already registered' });
+    }
+
+    const randomString = randomstring.generate();
+
+    const updatedUser = await prismaDB.user.update({
+      where: { id: currentUser.id },
+      data: {
+        name: user.name,
+        email: user.email,
+        emailVerified: isEmailNew ? false : currentUser.emailVerified,
+        emailVerificationString: isEmailNew ? randomString : null,
+      },
+    });
+
+    res.status(200).json({ success: true });
+
+    // send verification email
+    const verificationEmailMessage = await emailService.sendEmailVerification({
+      email: updatedUser.email,
+      id: updatedUser.id,
+      name: updatedUser.name,
+      randomString,
+    });
 
     return;
   } catch (err) {
--- server/src/middleware/user.middleware.ts
+++ server/src/middleware/user.middleware.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import { userPasswordUpdateModel } from '../models/user.model';
+import {
+  userDetailsUpdateModel,
+  userPasswordUpdateModel,
+} from '../models/user.model';
 import { JsonApiResponse } from '../constant-types';
 import { z } from 'zod';
 
@@ -12,6 +15,32 @@ export async function checkUserPasswordUpdateObjectValid(
     const { user } = req.body;
 
     userPasswordUpdateModel.parse(user);
+
+    next();
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.log(err.format());
+
+      return res
+        .status(400)
+        .json({ success: false, error: err.errors[0].message });
+    } else {
+      console.error(err);
+
+      next(err);
+    }
+  }
+}
+
+export async function checkUserDetailsUpdteObjectValid(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { user } = req.body;
+
+    userDetailsUpdateModel.parse(user);
 
     next();
   } catch (err) {

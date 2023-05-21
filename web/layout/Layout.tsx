94be67d2128834a2aedd182ad830d88dc8a2a247
arronrees <64908445+arronrees@users.noreply.github.com>
2023-05-21 10:59:25 +0100

    Updated layout component

--- web/layout/Layout.tsx
+++ web/layout/Layout.tsx
@@ -4,16 +4,16 @@ import useUser from '@/utils/iron/useUser';
 import { HashLoader } from 'react-spinners';
 import { useRouter } from 'next/router';
 
-type Props = { userNotRequired?: boolean; children: ReactNode };
+type LayoutProps = { userNotRequired?: boolean; children: ReactNode };
 
-export default function Layout({ userNotRequired, children }: Props) {
+export default function Layout({ userNotRequired, children }: LayoutProps) {
   const router = useRouter();
 
   const { user, isLoading } = useUser();
 
   if (isLoading) {
     return (
-      <div className='p-6 h-screen'>
+      <Wrapper className='h-screen'>
         <Header />
         <div
           className='flex items-center justify-center'
@@ -21,16 +21,16 @@ export default function Layout({ userNotRequired, children }: Props) {
         >
           <HashLoader color='#D0EBAE' />
         </div>
-      </div>
+      </Wrapper>
     );
   }
 
   if (userNotRequired) {
     return (
-      <div className='p-6'>
+      <Wrapper>
         <Header />
         <div className='pt-12'>{children}</div>
-      </div>
+      </Wrapper>
     );
   }
 
@@ -39,9 +39,22 @@ export default function Layout({ userNotRequired, children }: Props) {
   }
 
   return (
-    <div className='p-6'>
+    <Wrapper>
       <Header />
       <div className='pt-12'>{children}</div>
+    </Wrapper>
+  );
+}
+
+type WrapperProps = {
+  className?: string;
+  children: ReactNode;
+};
+
+export function Wrapper({ className, children }: WrapperProps) {
+  return (
+    <div className={`p-6 text-sm ${className ? className : ''}`}>
+      {children}
     </div>
   );
 }
